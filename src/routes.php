6e1eb996e14d35bffd4254be12584d26dabee6d4
Mart Pluijmaekers <mart@tuupke.nl>
2017-01-10 17:36:13 +0100

    Added functionality for granular includes and excludes

--- src/routes.php
+++ src/routes.php
@@ -1,79 +1,154 @@
 <?php
 
 Route::get(Config::get('swagger.doc-http-access')
-           . '/{environment?}/{page?}', function ($environment = null, $page = null) {
+    . '/{environment?}/{page?}',
+    function ($environment = null, $page = null) {
 
-    if (strpos($environment, ".json") !== false) {
-        $page = $environment;
-        $environment = null;
-    }
-
-    $env = environmentToWorking($environment);
-
-    $filePath = Config::get('swagger.docs-dir') . '/' . strtolower($env['name']) . "/{$page}";
-
-    if (File::extension($filePath) === "") {
-        $filePath .= ".json";
-    }
-
-    if (!File::Exists($filePath)) {
-        App::abort(404, "Cannot find {$filePath}");
-    }
-
-    $content = File::get($filePath);
-
-    return Response::make($content, 200, [
-        'Content-Type' => 'application/json',
-    ]);
-});
-
-Route::group(['prefix' => Config::get('swagger.doc-ui-access')], function () {
-    $defs = Config::get('swagger.swagger-defs');
-
-    foreach ($defs as $def => $settings) {
-        if (key_exists('routes', $settings) && get_class($settings['routes']) === \Closure::class) {
-            Route::group(['prefix' => $def], function () use ($settings) {
-                $settings['routes']();
-            });
+        if (strpos($environment, ".json") !== false) {
+            $page = $environment;
+            $environment = null;
         }
-    }
 
-    Route::get('{environment?}', function ($environment = null) {
         $env = environmentToWorking($environment);
 
-        $swagger_doc_dir = Config::get('swagger.docs-dir') . '/' . strtolower($env['name']);
-        //    return $env;
+        $filePath = Config::get('swagger.docs-dir') . '/' . strtolower($env['name']) . "/{$page}";
 
-        if (!File::exists($swagger_doc_dir) || is_writable($swagger_doc_dir)) {
-            if (File::exists($swagger_doc_dir)) {
-                File::deleteDirectory($swagger_doc_dir);
-            }
-
-            recurCreateFolder($swagger_doc_dir);
-
-            $excludeDirs = $env['excludes'] ?? [];
-            $appDir = $env['base-annotations-dir'] ?? Config::get('swagger.defaults.base-annotations-dir');
-
-            $swagger = \Swagger\scan($appDir, [
-                'exclude' => $excludeDirs,
-            ]);
-
-            $filename = $swagger_doc_dir . '/api-docs.json';
-            file_put_contents($filename, $swagger);
+        if (File::extension($filePath) === "") {
+            $filePath .= ".json";
         }
 
-        $callback = $env['view-renderer'] ?? function ($env) {
-                return view('package-swagger::main', [
-                    "api_docs" => '/' . Config::get('swagger.doc-http-access') . '/' . strtolower($env['name'])
-                                  . '/api-docs.json',
-                ]);
-            };
+        if (!File::Exists($filePath)) {
+            App::abort(404, "Cannot find {$filePath}");
+        }
 
-        return $callback($env);
+        $content = File::get($filePath);
+
+        return Response::make($content,
+            200,
+            [
+                'Content-Type' => 'application/json',
+            ]);
     });
-});
 
-function recurCreateFolder($path, $split = null) {
+Route::group(['prefix' => Config::get('swagger.doc-ui-access')],
+    function () {
+        $defs = Config::get('swagger.swagger-defs');
+
+        foreach ($defs as $def => $settings) {
+            if (key_exists('routes', $settings) && get_class($settings['routes']) === \Closure::class) {
+                Route::group(['prefix' => $def],
+                    function () use ($settings) {
+                        $settings['routes']();
+                    });
+            }
+        }
+
+        Route::get('{environment?}',
+            function ($environment = null) {
+                $env = environmentToWorking($environment);
+
+                $swagger_doc_dir = Config::get('swagger.docs-dir') . '/' . strtolower($env['name']);
+                //    return $env;
+
+                if (is_writable($swagger_doc_dir) && (!File::exists($swagger_doc_dir) || ($env['generateOnRequest'] || false))) {
+                    if (File::exists($swagger_doc_dir)) {
+                        File::deleteDirectory($swagger_doc_dir);
+                    }
+
+                    recurCreateFolder($swagger_doc_dir);
+
+                    $excludeDirs = includeResolver($env['excludes'] ?? []);
+                    $appDir = $env['base-annotations-dir'] ?? Config::get('swagger.defaults.base-annotations-dir');
+
+                    $swagger = \Swagger\scan($appDir,
+                        [
+                            'exclude' => $excludeDirs,
+                        ]
+                    );
+
+                    $filename = $swagger_doc_dir . '/api-docs.json';
+                    file_put_contents($filename, $swagger);
+                }
+
+                $callback = $env['view-renderer'] ?? function ($env) {
+                        return view('package-swagger::main',
+                            [
+                                "api_docs" => '/' . Config::get('swagger.doc-http-access') . '/' . strtolower($env['name'])
+                                    . '/api-docs.json',
+                            ]);
+                    };
+
+                return $callback($env);
+            });
+    });
+
+/*
+ * Build an array containing the excludes list
+ */
+function includeResolver (array $access) : array {
+    $excludesArray = [];
+    $wasExcluded = false;
+
+    foreach ($access as $item) {
+
+        $type = "excludes";
+        $value = null;
+
+        if (is_array($item)) {
+            $type = @$item["type"] ?? "excludes";
+            $value = @$item["value"];
+
+            if (is_null($value))
+                continue;
+        } else
+            $value = $item;
+
+        $val = valueToFileOrFolderArray($value);
+
+        switch ($type) {
+            case "includes":
+            case "include":
+            case "in":
+                if (!$wasExcluded)
+                    continue;
+
+                // Do the include operation
+                $excludesArray = array_diff($excludesArray, $val);
+
+                break;
+
+            case "excludes":
+            case "exclude":
+            case "ex":
+                $wasExcluded = true;
+
+                $excludesArray = array_unique(array_merge($excludesArray, $val));
+                break;
+        }
+    }
+
+    return $excludesArray;
+}
+
+function valueToFileOrFolderArray (string $value) : array {
+    if (!\file_exists($value))
+        return [];
+
+    if (!is_dir($value))
+        return [$value];
+
+    $val = scandir($value);
+
+    array_shift($val);
+    array_shift($val);
+
+    return array_map(function ($item) use ($value) {
+        return $value . '/' . $item;
+    },
+        $val);
+}
+
+function recurCreateFolder ($path, $split = null) {
     if (is_null($split))
         return recurCreateFolder($path, explode("/", $path));
 
@@ -89,7 +164,7 @@ function recurCreateFolder($path, $split = null) {
     File::makeDirectory($imploded . '/' . $top);
 }
 
-function environmentToWorking($environment) {
+function environmentToWorking ($environment) {
     $default = Config::get('swagger.default-swagger-def');
 
     $specs = Config::get('swagger.swagger-defs');
